a267e61c82cf0f943bf7cd856ed8fa033c25154d
Gansey14 <hlousek.samuel14@gmail.com>
2023-11-13 14:54:11 +0100

    Error correction in searchgame

--- src/pages/SearchPageV2.tsx
+++ src/pages/SearchPageV2.tsx
@@ -13,15 +13,14 @@ import {
 	IonList,
 } from "@ionic/react";
 import "./SearchPage.css";
-import CardSearchGame from "../components/CardSearchGame";
-import SearchInfo from "../components/CardSearchGame"
+import CardSearchGame, { SearchInfo } from "../components/CardSearchGame";
 import { db } from "./firebase-config"
-import { collection getDocs } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore"
 
 
 // Samuel, Jarl, Paolo - Paolo created a function which allowed us to list all the items. Samuel put it inside the page and adjusted the code so it works. Jarl started the search and filtering and Samuel finished the functionality.
 
-const SearchPageV2: React.FC = () => {
+const SearchPageV2: React.FC<SearchInfo> = () => {
 	const [searchTerm, setSearchTerm] = useState("");
 	const [filter, setFilter] = useState("");
 	const [games, setGames] = useState([]);
@@ -32,7 +31,7 @@ const SearchPageV2: React.FC = () => {
 		// function to list fetch games object from firebase
 		const getGames = async () => {
 			try {
-				const data = await getDocks(usersCollectionRef);
+				const data = await getDocs(gamesCollectionRef);
 				setGames(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
 			} catch (error) {
 				console.error("Error fetching data: ", error);
